404af3ed1a39145a09d1478cc06e5ca49b245f34
Tor E Hagemann <hagemt@rpi.edu>
2012-09-03 19:22:27 -0500

    Segmented testing

--- src/org/games/cards/poker/testing/TestRank.java
+++ src/org/games/cards/poker/testing/TestRank.java
@@ -1,34 +1,46 @@
-package com.shiva.cards.testing;
+package org.games.cards.poker.testing;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
-import com.shiva.cards.PokerRank;
+import org.games.cards.poker.PokerRank;
 
 /**
  * Test cases for non-trivial methods in the PokerRank class.
  * @author Tor E Hagemann <hagemt@rpi.edu>
- * @see com.shiva.cards.PokerRank#beats(com.shiva.cards.PokerRank)
+ * @see org.games.cards.poker.PokerRank#beats(org.games.cards.poker.PokerRank)
  */
 public class TestRank {
 	@Test
-	public void testBeatsPokerRankBoolean() {
-		// Try all PokerRanks against their corner cases
+	public void testBeatsPokerRankBooleanDiagonal() {
 		for (PokerRank r : PokerRank.values()) {
 			assertFalse(r.beats(r));
-			assertFalse(r.beats(PokerRank.JOKER));
-			if (r.isStandard() && !r.isAce()) {
-				assertTrue(PokerRank.ACE.beats(r));
-				assertFalse(PokerRank.ACE.beats(r, false));
-			}
 		}
+	}
+	@Test
+	public void testBeatsPokerRankBooleanJoker() {
+		for (PokerRank r : PokerRank.values()) {
+			assertFalse(r.beats(PokerRank.JOKER));
+		}
+	}
+	@Test
+	public void testBeatsPokerRankBooleanAce() {
+		for (PokerRank r : PokerRank.values()) {
+			if (r != PokerRank.JOKER && r != PokerRank.ACE) {
+				assertTrue(PokerRank.ACE.beats(r));
+			}
+			assertFalse(PokerRank.ACE.beats(r, false));
+		}
+	}
+	@Test
+	public void testBeatsPokerRankBooleanRandom() {
 		// Try a large number of random bounded comparisons
 		PokerRank[] ranks = PokerRank.values();
-		int values = ranks.length - 1;
+		int num_values = ranks.length - 1;
 		for (int i = 0; i < 1000000; ++i) {
 			// Index in the interval [1, ranks.length)
-			int a = (int)(Math.random() * values) + 1;
+			int a = (int)(Math.random() * num_values) + 1;
 			// Index in the interval [a, ranks.length)
-			int b = (int)(Math.random() * (values - a)) + a;
+			int b = (int)(Math.random() * (num_values - a)) + a;
 			// We require higher ranks to beat lower ones
 			assertFalse(ranks[a].beats(ranks[b], false));
 		}
